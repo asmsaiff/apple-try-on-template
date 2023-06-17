48c7d47133fe79855da904fa835d1ad7a7de10dc
asmsaiff <info@saifullah.co>
2023-06-17 13:26:43 +0600

    apple try on template design

---
+++ app.js
@@ -0,0 +1,92 @@
+// Import necessary files
+import Products from './products.js'
+
+// Selectors
+const colorVariantBtns = document.getElementById('color-varient')
+const colorVariantContainer = document.getElementById('color-variant-container')
+const productImage = document.getElementById('product-image')
+const gradColor = document.querySelectorAll('.grad-color')
+const groupProductImage = document.getElementById('group-product-img')
+const secondGrid = document.getElementById('2nd-grid-item')
+const thirdGrid = document.getElementById('3rd-grid-item')
+
+// OnScroll add-class to navigation
+// OnScroll event handler
+window.addEventListener('scroll', (e) => {
+    e.preventDefault()
+
+    if(window.scrollY > 100) {
+        colorVariantContainer.classList = 'header-bg fixed left-0 w-full top-14 py-3 z-50'
+        document.querySelector('.cvc').classList = 'cvc w-8/12 mx-auto flex gap-1 justify-end'
+    } else {
+        colorVariantContainer.classList = ''
+        document.querySelector('.cvc').classList = 'cvc flex gap-4'
+    }
+})
+
+// Create Buttons according to product color variants
+Products.forEach(product => {
+    var createBtn = document.createElement('button')
+    createBtn.classList = 'product-variants active w-7 h-7 rounded-full border border-1 flex items-center justify-center'
+    // createBtn.setAttribute('onclick', 'switchProduct( ' + product.id + ')')
+
+    var createSpan = document.createElement('span')
+    createSpan.classList = 'w-5 h-5 block rounded-full'
+    createSpan.style = 'background-color: ' + product.primaryColor
+
+    createBtn.appendChild(createSpan)
+    colorVariantBtns.appendChild(createBtn)
+})
+
+const productVariants = document.querySelectorAll('.product-variants');
+
+// Button Active
+productVariants.forEach(function(el, key){
+    el.addEventListener('click', function () {
+        el.classList.add("active");
+
+        productVariants.forEach(function(ell, els){
+            if(key !== els) {
+                ell.classList.remove('active');
+            }
+        });
+    });
+});
+
+// Change Product
+
+gradColor[0].classList.add('blue')
+
+productVariants.forEach((pv, key) => {
+    pv.addEventListener('click', () => {
+        // Product Change
+        productImage.style.opacity = '0'
+        groupProductImage.style.opacity = '0'
+        secondGrid.style.transform = 'translateX(1000px)'
+        thirdGrid.style.opacity = '0'
+
+        productImage.style.transition = '.9s all ease-in-out'
+        groupProductImage.style.transition = '.9s all ease-in-out'
+        secondGrid.style.transition = '.7s all ease-in-out'
+        thirdGrid.style.transition = '.9s all ease-in-out'
+
+        setTimeout(() => {
+            productImage.src = Products[key].path
+            groupProductImage.src = Products[key].groupImgPath
+            secondGrid.src = Products[key].path
+            thirdGrid.src = Products[key].brandImg
+        }, 800)
+        
+        setTimeout(() => {
+            productImage.style.opacity = '1'
+            groupProductImage.style.opacity = '1'
+            secondGrid.style.transform = 'translateX(0)'
+            thirdGrid.style.opacity = '1'
+
+            // Text color change
+            gradColor.forEach(gce => {
+                gce.style.backgroundImage = Products[key].gradColor
+            })
+        }, 900)
+    })
+})
---
+++ products.js
@@ -0,0 +1,40 @@
+const Products = [
+    {
+        id: 1,
+        name: "Arctic_Click_ Preview 1",
+        path: "files/previews-webp/Arctic_Click_ Preview 1.webp",
+        groupImgPath: "files/previews-webp/Image Group/Arctic_Click_ Preview 2 grp.png",
+        brandImg: "files/previews-webp/Image Group/Arctic_Click_ Preview 2.png",
+        primaryColor: '#48c7f5',
+        gradColor: "-webkit-linear-gradient(45deg, #48c7f5 60%, #266EC8)",
+    },
+    {
+        id: 2,
+        name: "Green_Click Preview 1",
+        path: "files/previews-webp/Green_Click Preview 1.webp",
+        groupImgPath: "files/previews-webp/Image Group/Green_Click Preview 2 grp.png",
+        brandImg: "files/previews-webp/Image Group/Green_Click Preview 2.png",
+        primaryColor: '#6fbf6b',
+        gradColor: "-webkit-linear-gradient(45deg, #6fbf6b 60%, #266EC8)"
+    },
+    {
+        id: 3,
+        name: "Scarlet_Click_ Preview 1",
+        path: "files/previews-webp/Scarlet_Click_ Preview 1.webp",
+        groupImgPath: "files/previews-webp/Image Group/Scarlet_Click_ Preview 2 grp.png",
+        brandImg: "files/previews-webp/Image Group/Scarlet_Click_ Preview 2.png",
+        primaryColor: '#ED2E46',
+        gradColor: "-webkit-linear-gradient(45deg, #ED2E46 60%, #266EC8)"
+    },
+    {
+        id: 4,
+        name: "Sunset_Click_ Preview 1",
+        path: "files/previews-webp/Sunset_Click_ Preview 1.webp",
+        groupImgPath: "files/previews-webp/Image Group/Sunset_Click_ Preview 2 grp.png",
+        brandImg: "files/previews-webp/Image Group/Sunset_Click_ Preview 2.png",
+        primaryColor: '#FFE455',
+        gradColor: "-webkit-linear-gradient(45deg, #FFE455 60%, #266EC8)"
+    }
+];
+
+export default Products
