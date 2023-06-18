b17762b585d0113ec8a1d5313de2083d37db93ce
asmsaiff <info@saifullah.co>
2023-06-18 11:25:03 +0600

    panel toggle design

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const gradColor = document.querySelectorAll('.grad-color')
 const groupProductImage = document.getElementById('group-product-img')
 const secondGrid = document.getElementById('2nd-grid-item')
 const thirdGrid = document.getElementById('3rd-grid-item')
+const panel = document.getElementById('panel')
+const panelImg = document.getElementById('panel-image')
 
 // OnScroll add-class to navigation
 // OnScroll event handler
@@ -75,6 +77,7 @@ productVariants.forEach((pv, key) => {
             groupProductImage.src = Products[key].groupImgPath
             secondGrid.src = Products[key].path
             thirdGrid.src = Products[key].brandImg
+            panelImg.src = Products[key].path
         }, 800)
         
         setTimeout(() => {
@@ -89,4 +92,41 @@ productVariants.forEach((pv, key) => {
             })
         }, 900)
     })
-})
+});
+
+// Panel Open
+const panelC = document.getElementById('panel-container')
+panelC.style.transform = "translateX('1000px')"
+document.addEventListener('click', e => {
+    if (e.target.parentElement.classList.contains('panel-open') || e.target.classList.contains('deep-dive-button-image-open')) {
+        panelC.classList = 'fixed translate-x-0 top-0 left-0 w-full h-screen z-50 transition duration-500'
+        document.body.classList.add("disable-scrolling");
+    } else if (e.target.parentElement.classList.contains('panel-close') || e.target.classList.contains('panel-close')) {
+        document.body.classList.remove("disable-scrolling");
+        panelC.classList = 'fixed translate-x-full top-0 left-0 w-full h-screen z-50 transition duration-500'
+        panelC.style.transform = "translateX('1000px')"
+    }
+});
+
+
+// Panel Scroller
+const container = document.getElementById('panel');
+const content = document.getElementById('panel-content');
+
+// Add event listener for mouse movement
+container.addEventListener('wheel', handleMouseWheel);
+
+// Handle mouse movement
+function handleMouseWheel(event) {
+    event.preventDefault();
+
+    const containerWidth = container.offsetWidth;
+    const contentWidth = content.offsetWidth;
+    const scrollWidth = contentWidth - containerWidth;
+
+    // Adjust the scroll position based on the wheel delta
+    const scrollDelta = Math.sign(event.deltaY) * 100;
+    const scrollPosition = Math.max(0, Math.min(container.scrollLeft + scrollDelta, scrollWidth));
+    
+    container.scrollLeft = scrollPosition;
+}
